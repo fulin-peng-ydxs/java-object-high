68283d799aa8706eb8f368f577e39ac339e87c49
pengfulin <1162855925@qq.com>
2023-10-24 11:59:17 +0800

    v4.4.7.2  -aes案例演示方法名完善

--- java-security/src/main/java/security/algorithm/equals/AesSecurity.java
+++ java-security/src/main/java/security/algorithm/equals/AesSecurity.java
@@ -21,9 +21,9 @@ public class AesSecurity {
         String transformation="AES";  //加密算法
         String algorithm="AES"; //密钥算法
         String data="硅谷"; //原文数据
-        String encryptDes = encryptDes(data, key, transformation, algorithm);
+        String encryptDes = encryptAes(data, key, transformation, algorithm);
         System.out.println("加密数据："+encryptDes);
-        String decryptDes = decryptDes(encryptDes, key, transformation, algorithm);
+        String decryptDes = decryptAes(encryptDes, key, transformation, algorithm);
         System.out.println("解密数据："+decryptDes);
     }
 
@@ -35,7 +35,7 @@ public class AesSecurity {
      * @Param [data：原文, key：密钥, transformation：加密算法, algorithm：密钥算法]
      * @return 密文
      **/
-    private  static String encryptDes(String data,String key,String transformation,String algorithm) throws Exception {
+    private  static String encryptAes(String data,String key,String transformation,String algorithm) throws Exception {
         //获取安全对象
         Cipher cipher = Cipher.getInstance(transformation);
         //创建加密规则
@@ -56,7 +56,7 @@ public class AesSecurity {
      * @Param [data：原文, key：密钥, transformation：解密算法, algorithm：密钥算法]
      * @return 原文
      **/
-    private static String decryptDes(String data,String key,String transformation,String algorithm) throws Exception{
+    private static String decryptAes(String data,String key,String transformation,String algorithm) throws Exception{
         //获取安全对象
         Cipher cipher = Cipher.getInstance(transformation);
         //创建解密规则

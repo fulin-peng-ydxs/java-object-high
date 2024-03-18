de21f5f31f2c211f008bd20e278e683e07ac64b6
pengfulin <1162855925@qq.com>
2024-03-18 17:51:23 +0800

    security补充JWT的使用

--- java-security/src/main/java/security/jwt/JwtQuick.java
+++ java-security/src/main/java/security/jwt/JwtQuick.java
@@ -1,8 +1,9 @@
 package security.jwt;
 
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.security.Keys;
-
 import javax.crypto.SecretKey;
 import java.util.Date;
 
@@ -19,6 +20,9 @@ public class JwtQuick {
     public static void main(String[] args) {
         String jwt = generate(secretString);
         System.out.println("Generated JWT: " + jwt);
+        Jws<Claims> claims = resolve(jwt);
+        System.out.println("Subject: " + claims.getBody().getSubject());
+        System.out.println("Expiration: " + claims.getBody().getExpiration());
     }
 
     /**
@@ -33,13 +37,25 @@ public class JwtQuick {
         Date exp = new Date(expMillis);
         // 对于 HMAC ,使用你的安全密钥。使用正确的长度对于安全性至关重要。
         SecretKey secretKey = Keys.hmacShaKeyFor(secretString.getBytes());
-        String jwt = Jwts.builder()
+        return Jwts.builder()
                 .setSubject("user123") // 设置主题或者说用户ID
                 .setIssuedAt(now) // 设置签发时间
                 .setExpiration(exp) // 设置过期时间
                 .signWith(secretKey) // 设置签名算法和密钥
-                .compact(); // 将 JWT 压缩为一个安全的 URL 字符串
-        return jwt;
+                .compact();
+    }
+
+    /**
+     * 解析JWT
+     * 2024/3/18 0018 17:35
+     * @author fulin-peng
+     */
+    public static Jws<Claims> resolve(String jwt){
+        SecretKey secretKey = Keys.hmacShaKeyFor(secretString.getBytes());
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(jwt);
     }
 
 }

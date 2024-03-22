9acd6d9533d88f7912a27e02ae7e9407cc0e8e4e
pengfulin <1162855925@qq.com>
2024-03-22 11:41:35 +0800

    -servlet3.0新特性补充完善

--- java-web/src/main/java/web/servlet/newsdk/AsyncServletQuick.java
+++ java-web/src/main/java/web/servlet/newsdk/AsyncServletQuick.java
@@ -1,13 +1,11 @@
 package web.servlet.newsdk;
 
 import javax.servlet.AsyncContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * @author PengFuLin
@@ -18,29 +16,28 @@ import java.io.IOException;
 
 //1、支持异步处理asyncSupported=true
 @WebServlet(value="/async",asyncSupported=true)
-public class AsyncServletDemo extends HttpServlet {
+public class AsyncServletQuick extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         System.out.println("主线程开始。。。"+Thread.currentThread()+"==>"+System.currentTimeMillis());
         //2、开启异步模式
         AsyncContext startAsync = req.startAsync();
-
         //3、业务逻辑执行异步处理;
-        startAsync.start(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    System.out.println("副线程开始。。。"+Thread.currentThread()+"==>"+System.currentTimeMillis());
-                    sayHello();
-                    startAsync.complete();
-                    //获取到异步上下文
-                    AsyncContext asyncContext = req.getAsyncContext();
-                    //4、获取响应
-                    ServletResponse response = asyncContext.getResponse();
-                    response.getWriter().write("hello async...");
-                    System.out.println("副线程结束。。。"+Thread.currentThread()+"==>"+System.currentTimeMillis());
-                } catch (Exception e) {
-                }
+        startAsync.start(() -> {
+            try {
+                System.out.println("副线程开始。。。"+Thread.currentThread()+"==>"+System.currentTimeMillis());
+                sayHello();
+                //获取到异步上下文或直接使用startAsync
+                //AsyncContext asyncContext = req.getAsyncContext();
+                // 获取响应
+                ServletResponse response = startAsync.getResponse();
+                response.getWriter().write("hello async...");
+                System.out.println("副线程结束。。。"+Thread.currentThread()+"==>"+System.currentTimeMillis());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }finally {
+                // 在所有操作完成后，最后调用complete()
+                startAsync.complete();
             }
         });
         System.out.println("主线程结束。。。"+Thread.currentThread()+"==>"+System.currentTimeMillis());
@@ -50,5 +47,4 @@ public class AsyncServletDemo extends HttpServlet {
         System.out.println(Thread.currentThread()+" processing...");
         Thread.sleep(3000);
     }
-
 }
